535115a9533fe2761fb37f6fa88f517b781c560b
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-04-28 13:22:29 +0600

    orders data send successfully to the database

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../Firebase.init";
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [user, loading, error] = useAuthState(auth);
   useEffect(() => {
     fetch("http://localhost:5000/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleOrder = (product) =>{
+    const {name, price} = product;
+    //  console.log(product, user.email)
+    fetch('http://localhost:5000/addOrder', {
+      method: 'POST',
+      body: JSON.stringify({
+           name,
+           price,
+           email: user.email
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) =>{
+          console.log(json)
+        
+      });
+  }
   return (
     <div className="container">
       <h2>Total products: {products.length}.</h2>
@@ -18,7 +42,7 @@ const Products = () => {
               <h5 class="card-title">{pd.name}</h5>
               <h6 class="card-subtitle mb-2 text-muted">price: {pd.price}</h6>
              
-              <button className="btn btn-link" href="#" class="card-link">
+              <button onClick={()=>handleOrder(pd)} className="btn btn-link" href="#" class="card-link">
                  Order Now
               </button>
              
